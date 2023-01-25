f5d54ca59577a51c6ccaafc45ad36867660a91e5
Gisele <giselebastin.webstyle@gmail.com>
2023-01-25 10:15:56 +0100

    models + migrations for bank accounts and statements

--- myBank/database/migrations/2023_01_25_090305_create_accounts_table.php
+++ myBank/database/migrations/2023_01_25_090305_create_accounts_table.php
@@ -15,6 +15,20 @@ return new class extends Migration
     {
         Schema::create('accounts', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('accountNumber');
+            $table->string('city');
+            $table->string('email')->unique();
+            $table->integer('balance');
+            $table->integer('phone');
+            $table->integer('cmic');
+            $table->string('accountType');
+            $table->string('address');
+            $table->string('password');
+            $table->string('source');
+            $table->string('photo');
+            $table->string('branchName');
+            $table->string('branchCode');
             $table->timestamps();
         });
     }
